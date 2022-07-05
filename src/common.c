d1886e54dbcc8dffd79e051e1ed81448353a3d84
21pages <pages21@163.com>
2022-07-05 08:56:03 +0800

    fix linux build warning
    Signed-off-by: 21pages <pages21@163.com>

--- src/common.c
+++ src/common.c
@@ -5,8 +5,8 @@ void my_fprintf(FILE *const _Stream, const char *const _Format, ...) {
   va_list ap;
   int level = av_log_get_level();
 
-  if (_Stream == stderr && level >= AV_LOG_ERROR ||
-      _Stream == stdout && level > AV_LOG_QUIET) {
+  if ((_Stream == stderr && level >= AV_LOG_ERROR) ||
+      (_Stream == stdout && level > AV_LOG_QUIET)) {
     va_start(ap, _Format);
     vfprintf(_Stream, _Format, ap);
     va_end(ap);
