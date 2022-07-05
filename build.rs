adda85a83b2ed64961ad816b06f22aced7c24d71
21pages <pages21@163.com>
2022-07-05 08:55:17 +0800

    build linux
    Signed-off-by: 21pages <pages21@163.com>

--- build.rs
+++ build.rs
@@ -12,14 +12,29 @@ fn main() {
         .write_to_file(Path::new(&env::var_os("OUT_DIR").unwrap()).join("ffi.rs"))
         .unwrap();
 
-    println!("cargo:rustc-link-search=native=ffmpeg/windows/release/lib");
-    let static_libs = ["avcodec", "avfilter", "avutil", "mfx"];
-    let dyn_libs = ["User32", "bcrypt", "ole32", "advapi32"];
-    static_libs.map(|lib| println!("cargo:rustc-link-lib=static={}", lib));
-    dyn_libs.map(|lib| println!("cargo:rustc-link-lib={}", lib));
+    let mut builder = Build::new();
 
-    Build::new()
-        .include("ffmpeg/windows/release/include")
+    #[cfg(target_os = "windows")]
+    {
+        println!("cargo:rustc-link-search=native=ffmpeg/windows/release/lib");
+        let static_libs = ["avcodec", "avfilter", "avutil", "mfx"];
+        static_libs.map(|lib| println!("cargo:rustc-link-lib=static={}", lib));
+        let dyn_libs = ["User32", "bcrypt", "ole32", "advapi32"];
+        dyn_libs.map(|lib| println!("cargo:rustc-link-lib={}", lib));
+        builder.include("ffmpeg/windows/release/include");
+    }
+
+    #[cfg(target_os = "linux")]
+    {
+        println!("cargo:rustc-link-search=native=ffmpeg/linux/release/lib");
+        let static_libs = ["avcodec", "avfilter", "avutil"];
+        static_libs.map(|lib| println!("cargo:rustc-link-lib=static={}", lib));
+        let dyn_libs = ["va", "va-drm", "va-x11", "vdpau", "X11"];
+        dyn_libs.map(|lib| println!("cargo:rustc-link-lib={}", lib));
+        builder.include("ffmpeg/linux/release/include");
+    }
+
+    builder
         .file("src/encode.c")
         .file("src/decode.c")
         .file("src/common.c")
