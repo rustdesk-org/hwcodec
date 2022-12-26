c0c6dc7b42332c169fe74ed741f479b8e10792ef
KG7x <Cados7x@gmail.com>
2022-12-26 07:07:39 +0300

    return ffmpeg typo )

--- ffmpeg/linux/release/include/libavutil/log.h
+++ ffmpeg/linux/release/include/libavutil/log.h
@@ -247,9 +247,9 @@ void av_log(void *avcl, int level, const char *fmt, ...) av_printf_format(3, 4);
  * @param avcl A pointer to an arbitrary struct of which the first field is a
  *        pointer to an AVClass struct or NULL if general log.
  * @param initial_level importance level of the message expressed using a @ref
- *        lavu_log_constants "Logging Constant" for the first occurrence.
+ *        lavu_log_constants "Logging Constant" for the first occurance.
  * @param subsequent_level importance level of the message expressed using a @ref
- *        lavu_log_constants "Logging Constant" after the first occurrence.
+ *        lavu_log_constants "Logging Constant" after the first occurance.
  * @param fmt The format string (printf-compatible) that specifies how
  *        subsequent arguments are converted to output.
  * @param state a variable to keep trak of if a message has already been printed
